c3845b2547cde1f8a88f25cadb984208dfd7a48e
Steven Nunez <octosteve@github.com>
2021-04-16 00:41:14 -0400

    Wrap form in frame 🤞🏼

--- lib/third_rail_web/controllers/comment_controller.ex
+++ lib/third_rail_web/controllers/comment_controller.ex
@@ -3,21 +3,42 @@ defmodule ThirdRailWeb.CommentController do
 
   alias ThirdRail.Core
 
+  plug :can_handle_turbo_stream
+
   def index(conn, %{"issue_id" => issue_id}) do
     comments = Core.list_comments_for_issue(issue_id)
-    render(conn, "index.html", comments: comments)
+    render(conn, "index.html", comments: comments, layout: false)
   end
 
   def create(conn, %{"issue_id" => issue_id, "comment" => comment_params}) do
     case Core.create_comment_for_issue(issue_id, comment_params) do
-      {:ok, _comment} ->
-        conn
-        |> put_flash(:info, "Comment Successfully created")
-        |> redirect(to: Routes.issue_path(conn, :show, issue_id))
+      {:ok, comment} ->
+        if Map.get(conn.assigns, :can_handle_turbo_stream) do
+          conn
+          |> render("comment.stream.html", comment: comment, layout: false)
+        else
+          conn
+          |> put_flash(:info, "Comment Successfully created")
+          |> redirect(to: Routes.issue_path(conn, :show, issue_id))
+        end
 
       {:error, issue, %Ecto.Changeset{}} ->
         conn
         |> redirect(to: Routes.issue_path(conn, :show, issue))
+    end
+  end
+
+  defp can_handle_turbo_stream(conn, _opts) do
+    case conn |> get_req_header("accept") do
+      [accept] ->
+        if String.contains?(accept, "text/vnd.turbo-stream.html") do
+          conn |> assign(:can_handle_turbo_stream, true)
+        else
+          conn
+        end
+
+      [] ->
+        conn
     end
   end
 end
